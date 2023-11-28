3b254ce23ecdc377799d0d246050d30227346a78
Young Park <minyeoungpark6929@gmail.com>
2023-11-28 20:45:49 +0900

    Lv1 upgraded

--- app/src/main/java/com/example/mycalcurator/Calculator.kt
+++ app/src/main/java/com/example/mycalcurator/Calculator.kt
@@ -1,30 +1,15 @@
 package com.example.mycalcurator
 
 class Calculator {
-    fun calculator(num1: Int, num2: Int, op: Int): Int {
-        var result = 0
+    fun calculator(num1: Double, num2: Double, op: Int): Double {
+        var result:Double = 0.0
         when (op) {
-            1 -> {
-                result = num1 + num2
-            }
-
-            2 -> {
-                result = num1 - num2
-            }
-
-            3 -> {
-                result = num1 * num2
-            }
-
-            4 -> {
-                result = num1 / num2
-            }
-
-            else -> {
-                result = -1
-            }
+            1 -> {result = num1 + num2}
+            2 -> {result = num1 - num2}
+            3 -> {result = num1 * num2}
+            4 -> {result = num1 / num2}
+            else -> {result = -1.0}
         }
-
         return result
     }
 }
--- app/src/main/java/com/example/mycalcurator/main.kt
+++ app/src/main/java/com/example/mycalcurator/main.kt
@@ -3,9 +3,9 @@ package com.example.mycalcurator
 fun main() {
     println("-------------------계산기-------------------")
     println("--------연산할 첫 번째 숫자를 입력하세요--------")
-    val num1 = readLine()!!.toInt()
+    val num1 = readLine()!!.toDouble()
     println("--------연산할 두 번째 숫자를 입력하세요--------")
-    val num2 = readLine()!!.toInt()
+    val num2 = readLine()!!.toDouble()
     println("-------연산자에 해당하는 숫자를 입력하세요-------")
     println("-----1(더하기) 2(빼기) 3(곱하기) 4(나누기)-----")
     val op = readLine()!!.toInt()
