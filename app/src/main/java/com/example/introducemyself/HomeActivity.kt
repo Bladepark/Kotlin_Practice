b9822c1a4b472d1b465cb7b3b43e26a237e43e0b
Young Park <minyeoungpark6929@gmail.com>
2023-12-13 22:48:42 +0900

    Lv4 auto login tasking Lv5 complete

---
+++ app/src/main/java/com/example/introducemyself/HomeActivity.kt
@@ -0,0 +1,50 @@
+package com.example.introducemyself
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val homeImgView = findViewById<ImageView>(R.id.img_home)
+        val idTextView = findViewById<TextView>(R.id.tv_homeId)
+        val nameTextView = findViewById<TextView>(R.id.tv_homeName)
+        val ageTextView = findViewById<TextView>(R.id.tv_homeAge)
+        val mbtiTextView = findViewById<TextView>(R.id.tv_homeMbti)
+        val homeExitBtn = findViewById<Button>(R.id.btn_homeExit)
+
+        val id = intent.getStringExtra("memberId")
+        val pwd = intent.getStringExtra("memberPwd")
+
+        val memberInfo = MemberInfo.memberInfo.find { it.id == id && it.pwd == pwd }
+
+        val imgResources = arrayOf(R.drawable.bbangbbang1,R.drawable.bbangbbang2,R.drawable.bbangbbang3,
+            R.drawable.bbangbbang4,R.drawable.bbangbbang5,R.drawable.bbangbbang6,R.drawable.bbangbbang7)
+
+        val random = Random.nextInt(imgResources.size)
+
+        homeImgView.setImageResource(imgResources[random])
+        idTextView.setText("아이디 :" + memberInfo?.id)
+        nameTextView.setText("이름 :" + memberInfo?.name)
+        ageTextView.setText("나이 :" + memberInfo?.age)
+        mbtiTextView.setText("MBTI :" + memberInfo?.mbti)
+        MemberInfo.memberInfo.forEach {
+            println("$it")
+        }
+        homeExitBtn.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.putExtra("memberId", id)
+            intent.putExtra("memberPwd", pwd)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/introducemyself/SignUpActivity.kt
+++ app/src/main/java/com/example/introducemyself/SignUpActivity.kt
@@ -1,11 +1,40 @@
 package com.example.introducemyself
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val nameEditText = findViewById<EditText>(R.id.et_name)
+        val idEditText = findViewById<EditText>(R.id.et_signUpID)
+        val pwdEditText = findViewById<EditText>(R.id.et_signUpPwd)
+        val signUpBtn = findViewById<Button>(R.id.btn_signUp)
+
+        signUpBtn.setOnClickListener {
+            val inputName = nameEditText.text.toString()
+            val inputId = idEditText.text.toString()
+            val inputPwd = pwdEditText.text.toString()
+            if(nameEditText.text.isEmpty()||pwdEditText.text.isEmpty()||idEditText.text.isEmpty()){
+                Toast.makeText( this,"이름, 아이디, 비밀번호 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, AddInfoActivity::class.java)
+                intent.putExtra("memberName",inputName)
+                intent.putExtra("memberId",inputId)
+                intent.putExtra("memberPwd",inputPwd)
+                startActivity(intent)
+            }
+        }
+
+
+
+
     }
 }
