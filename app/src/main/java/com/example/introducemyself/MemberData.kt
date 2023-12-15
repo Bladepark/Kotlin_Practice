2f8399eb8e9d0e61fee7351b994df6392c040acd
Young Park <minyeoungpark6929@gmail.com>
2023-12-15 15:57:55 +0900

    branch alive

--- app/src/main/java/com/example/introducemyself/MemberData.kt
+++ app/src/main/java/com/example/introducemyself/MemberData.kt
@@ -1,3 +1,3 @@
 package com.example.introducemyself
 
-data class MemberData(val id: String, val pwd: String, val name:String, var mbti : String)
+data class MemberData(val id: String, val pwd: String, val name:String, val age : String, var mbti : String)
