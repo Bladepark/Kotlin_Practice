8fccc2ec34efffe9412dba8900100c4d5dfc322d
Young Park <minyeoungpark6929@gmail.com>
2024-01-12 11:46:03 +0900

    add bottom navigation , navController and Fragments

--- Standard_YouTube/app/src/main/java/com/example/youtube/MainActivity.kt
+++ Standard_YouTube/app/src/main/java/com/example/youtube/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.youtube.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // 바텀 네비게이션 뷰 바인딩
+        val bottomNav: BottomNavigationView = binding.bottomNavigationView
+
+        // fragmentContainerView 뷰 바인딩 및 navController 설정
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // 바텀 네비게이션과 네비게이션 컨트롤러 연결
+        bottomNav.setupWithNavController(navController)
     }
 }
